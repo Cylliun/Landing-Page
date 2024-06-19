a835fa13ae36a4e907cfac8ea8006a4c2e050549
Cylliun <japaguilherme55@gmail.com>
2024-06-19 16:04:15 -0400

    titulo e pequeno texto da empresa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyle from "./styled";
 import Header from "./components/Header/header";
+import Section from "./components/Section/section"
 
 function App() {
   return (
     <>
       <GlobalStyle/>
       <Header />
+      <Section />
     </>
   );
 }
