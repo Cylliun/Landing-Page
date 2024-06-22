3c64386c81fe74b278dfe7416aae827c8920ae88
Cylliun <japaguilherme55@gmail.com>
2024-06-22 01:23:33 -0400

    novos arquivos tipo ts

---
+++ src/components/Section/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const AboutCompany = styled.main`
+
+    *{
+        display: flex;
+        text-align:justify;
+        justify-content: center;
+        padding: 10px 10px;
+        align-items: center;
+        background-color: #989A91;
+        color:black;
+    }
+
+    .table{
+        display:flex;
+        flex-direction: column;
+    }
+
+    .table.table-text{
+        
+        padding: 15%;
+    }
+
+`;
+
+export default AboutCompany;
